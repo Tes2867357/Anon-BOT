4b30c46138e3de8ac12cb3619972db6ca8ac4666
Tes108080 <101046817+Tes108080@users.noreply.github.com>
2022-07-12 01:09:11 +0800

    Update main.js

--- main.js
+++ main.js
@@ -100,7 +100,7 @@ async function connectionUpdate(update) {
   if (connection == 'connecting') console.log(chalk.redBright('🕛 Mengaktifkan Bot, Harap tunggu sebentar...'))
   if (connection == 'open') {
       console.log(chalk.green('Connected✅'))
-      await conn.hehe("6285156299020@s.whatsapp.net", global.ftoli)
+      await conn.hehe("6281238772258@s.whatsapp.net", global.ftoli)
   }
   if (connection == 'close') console.log(chalk.red('⏹️Koneksi berhenti dan mencoba menghubungkan kembali...'))
   global.timestamp.connect = new Date
